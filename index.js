2e86862b5ebc41d4a1ef5a91df1f32b48d388be9
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 04:50:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function (){}
+function (){}
+function (){}
+function (){}
+function (){}
+function (){}
+function (){}
+function (){}
