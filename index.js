ca62b0604c1a5a848d7a58ff84f0ebb8cf765518
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 04:50:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){}
-function (){}
+function destructivelyPrependKitten(name){}
 function (){}
 function (){}
 function (){}
